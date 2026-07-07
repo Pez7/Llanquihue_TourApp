e4c7623a0dba4bf9ab6cbfc522896f9636cb26d2
MariPaz7 <maria.pazrojasc@gmail.com>
2026-07-06 21:11:14 -0400

    se actualiza archivo Main

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,37 +1,23 @@
 
 package ui;
-import model.ExcursionCultural;
-import model.PaseoLacustre;
-import model.RutaGastronomica;
 import model.ServicioTuristico;
 import data.GestorServicios;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
         GestorServicios gestor = new GestorServicios();
         
-        ServicioTuristico servicio1 = gestor.excursionUno();
-        ServicioTuristico servicio2 = gestor.excursionDos();
-        ServicioTuristico servicio3 = gestor.paseoUno();
-        ServicioTuristico servicio4 = gestor.paseoDos();
-        ServicioTuristico servicio5 = gestor.rutaUno();
-        ServicioTuristico servicio6 = gestor.rutaDos();
+        ArrayList <ServicioTuristico> servicios = gestor.generarServicio();
         
-        System.out.println("=== Listado de servicios LlanquihueTour ===");
-        System.out.println();
-
-        System.out.println(servicio1);
-        System.out.println();
-        System.out.println(servicio2);
-        System.out.println();
-        System.out.println(servicio3);
-        System.out.println();
-        System.out.println(servicio4);
-        System.out.println();
-        System.out.println(servicio5);
-        System.out.println();
-        System.out.println(servicio6);
+        System.out.println("-------Lista de Servicios de LlanquihueTour-------");
+        
+        for(ServicioTuristico servicio : servicios){
+            
+           servicio.mostrarInformacion();
+        }
+        
     }
     
 }
