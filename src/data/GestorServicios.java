5bb384a0684f7a77eea643d7789a7288032602d3
MariPaz7 <maria.pazrojasc@gmail.com>
2026-07-06 21:05:07 -0400

    se actualiza la clase GestorServicios

--- src/data/GestorServicios.java
+++ src/data/GestorServicios.java
@@ -1,29 +1,26 @@
 
 package data;
+
 import model.ExcursionCultural;
 import model.PaseoLacustre;
 import model.RutaGastronomica;
 import model.ServicioTuristico;
+import java.util.ArrayList;
 
 
 public class GestorServicios {
     
-        public ServicioTuristico excursionUno() {
-        return new ExcursionCultural("Fuerte Bulnes","Caminata",2);
-    }
-        public ServicioTuristico excursionDos() {
-        return new ExcursionCultural("Iglesias de Chiloé","Paseo en bicicleta",3);
-    }
-        public ServicioTuristico paseoUno() {
-        return new PaseoLacustre("Lancha a motor", "Paseo por el Lago Llanquihue", 2);
-    }
-        public ServicioTuristico paseoDos() {
-        return new PaseoLacustre("Ferri", "Cruce Puerto Montt - Bariloche",4);
-    }
-        public ServicioTuristico rutaUno() {
-        return new RutaGastronomica(3,"Ruta del Salmón",2);
-    }
-        public ServicioTuristico rutaDos() {
-        return new RutaGastronomica(2,"Ruta de la Cerveza",3);
+    public ArrayList<ServicioTuristico> generarServicio(){
+
+        ArrayList<ServicioTuristico> servicio = new ArrayList<>();
+        
+        servicio.add(new ExcursionCultural("Fuerte Bulnes","Caminata",2));
+        servicio.add(new PaseoLacustre("Lancha a motor", "Paseo por el Lago Llanquihue", 2));
+        servicio.add(new RutaGastronomica(3,"Ruta del Salmón",2));
+        servicio.add(new PaseoLacustre("Ferri", "Cruce Puerto Montt - Bariloche",4));
+        servicio.add(new ExcursionCultural("Iglesias de Chiloé","Paseo en bicicleta",3));
+
+        return servicio;
     }
 }
+
